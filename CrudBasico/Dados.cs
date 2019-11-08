149a9f4922cb135d4efaca60d6cd7674ffb14492
Thiago Moura <thiago.mouraluzia@gmail.com>
2019-11-08 07:42:35 -0300

    Foi criado o banco, a aplicação, foi configurado o banco na aplicação e criado a classe de Acesso a Dados.

---
+++ CrudBasico/Dados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CrudBasico
+{
+    public class Dados
+    {
+        //Variavel que recebera a string de conexao
+        public string strConexao = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        //variaveis constantes que conteram as instrucoes sql para o crud
+        public const string strInsert = "INSERT INTO Clientes VALUES (@Nome, @Endereco, @Telefone, @Sexo, @Ativo, @DataCadastro";
+        public const string strDelete = "DELETE FROM Clientes WHERE IdCliente = @IdCliente";
+        public const string strUpdate = "UPDATE Clientes SET Nome = @Nome, Endereco = @Endereco, Telefone = @Telefone, Sexo = @Sexo, Ativo = @Ativo, DataCadastro = @DataCadastro WHERE IdCliente = @IdCliente";
+        public const string strSelect = "SELECT * FROM Clientes";
+    }
+}
