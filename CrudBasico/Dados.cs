a4c4af6b8458814167c6e37bb8be17fb8eb8cce1
Thiago Moura <thiago.mouraluzia@gmail.com>
2019-11-08 08:10:11 -0300

    foram criado os métodos Alterar, Gravar e Excluir.

--- CrudBasico/Dados.cs
+++ CrudBasico/Dados.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CrudBasico
 {
@@ -17,5 +20,59 @@ namespace CrudBasico
         public const string strDelete = "DELETE FROM Clientes WHERE IdCliente = @IdCliente";
         public const string strUpdate = "UPDATE Clientes SET Nome = @Nome, Endereco = @Endereco, Telefone = @Telefone, Sexo = @Sexo, Ativo = @Ativo, DataCadastro = @DataCadastro WHERE IdCliente = @IdCliente";
         public const string strSelect = "SELECT * FROM Clientes";
+
+        public void Gravar(string Nome, string Endereco, string Telefone, string Sexo, bool Ativo, DateTime DataCadastro)
+        {
+            using (SqlConnection objetoConexao = new SqlConnection(strConexao))
+            {
+                using(SqlCommand objetoComando = new SqlCommand(strInsert, objetoConexao))
+                {
+                    objetoComando.Parameters.AddWithValue("@Nome", Nome);
+                    objetoComando.Parameters.AddWithValue("@Endereco", Endereco);
+                    objetoComando.Parameters.AddWithValue("@Telefone", Telefone);
+                    objetoComando.Parameters.AddWithValue("@Sexo", Sexo);
+                    objetoComando.Parameters.AddWithValue("@Ativo", Ativo);
+                    objetoComando.Parameters.AddWithValue("@DataCadastro", DataCadastro);
+
+                    objetoConexao.Open();
+                    objetoComando.ExecuteNonQuery();
+                    objetoConexao.Close();
+                }
+            }
+        }
+        public void Atualizar(int IdCliente,string Nome, string Endereco, string Telefone, string Sexo, bool Ativo, DateTime DataCadastro)
+        {
+            using (SqlConnection objetoConexao = new SqlConnection(strConexao))
+            {
+                using (SqlCommand objetoComando = new SqlCommand(strUpdate, objetoConexao))
+                {
+                    objetoComando.Parameters.AddWithValue("@IdCliente", IdCliente);
+                    objetoComando.Parameters.AddWithValue("@Nome", Nome);
+                    objetoComando.Parameters.AddWithValue("@Endereco", Endereco);
+                    objetoComando.Parameters.AddWithValue("@Telefone", Telefone);
+                    objetoComando.Parameters.AddWithValue("@Sexo", Sexo);
+                    objetoComando.Parameters.AddWithValue("@Ativo", Ativo);
+                    objetoComando.Parameters.AddWithValue("@DataCadastro", DataCadastro);
+
+                    objetoConexao.Open();
+                    objetoComando.ExecuteNonQuery();
+                    objetoConexao.Close();
+                }
+            }
+        }
+        public void Excluir(int IdCliente)
+        {
+            using (SqlConnection objetoConexao = new SqlConnection(strConexao))
+            {
+                using (SqlCommand objetoComando = new SqlCommand(strDelete, objetoConexao))
+                {
+                    objetoComando.Parameters.AddWithValue("@idCliente", IdCliente);
+
+                    objetoConexao.Open();
+                    objetoComando.ExecuteNonQuery();
+                    objetoConexao.Close();
+                }
+            }
+        }
     }
 }
